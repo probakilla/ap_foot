8d44715215f2e2c66470180c524ea78f55133e7a
Julien Pilleux <julien.pilleux@etu.u-bordeaux.fr>
2018-10-19 10:39:48 +0200

    Ajout du traitement des inputs

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import sys
+from graph import buildGraph
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("ERROR : NEED A JSON CONFIGURATION FILE!", file=sys.stderr)
+        sys.exit()
     graph = buildGraph(sys.argv[1])
