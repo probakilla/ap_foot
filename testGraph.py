bf2e89d58f84d835dcbc342f7a9af5dc5276a661
gchupin <guillaume.chupin@etu.u-bordeaux.fr>
2018-11-04 14:26:42 +0100

    Ajout du test de searchDominatingSet

--- testGraph.py
+++ testGraph.py
@@ -1,6 +1,7 @@
 import unittest
 from graph import Graph
-
+from node import BLACK, AtkNode, DefNode
+from algo import searchDominatingSet
 
 class TestGraphMethods(unittest.TestCase):
 
@@ -20,7 +21,24 @@ class TestGraphMethods(unittest.TestCase):
         return
 
     def testSearchDominatingSet(self):
-        return
+        AtkA = AtkNode ((1, 0))
+        AtkB = AtkNode ((2, 0))
+        AtkC = AtkNode ((3, 0))
+        DefA = DefNode ((0, 1))
+        DefB = DefNode ((0, 2))
+        DefC = DefNode ((0, 3))
+        graphDict = {
+            AtkA: [DefA, DefB],
+            AtkB: [DefA, DefC],
+            AtkC: [DefB, DefC],
+            DefA: [AtkA, AtkB],
+            DefB: [AtkA, AtkC],
+            DefC: [AtkB, AtkC]
+        }
+        g = Graph (graphDict)
+        dominatingSet = searchDominatingSet (g, 3)
+        expectedDominatingSet = [DefA, DefC] #Can be any couple of defenders
+        self.assertEqual (dominatingSet, expectedDominatingSet)
 
     def testRemainsUndominateAttacker(self):
         return
