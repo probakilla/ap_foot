9d145512405c82f2777b0024a7c9e179765c87f7
gchupin <guillaume.chupin@etu.u-bordeaux.fr>
2018-11-07 22:34:41 +0100

    Correction mineur

--- algo.py
+++ algo.py
@@ -4,34 +4,35 @@ from node import BLACK, WHITE, AtkNode, DefNode
 from graph import Graph
 
 def DSAP (graph, k, removedNodes = []):
+    graphCpy = copy.deepcopy (graph)
     cont = True
     while k > 0 and cont:
         tmp = False
-        tmp = graph.removeEdgeBetweenTwoWhiteNodes () or tmp
-        tmp = graph.removeWhiteNodeWithBlackNeighboorhood () or tmp
-        tmp2 = graph.R3 (k)
+        tmp = graphCpy.removeEdgeBetweenTwoWhiteNodes () or tmp
+        tmp = graphCpy.removeWhiteNodeWithBlackNeighboorhood () or tmp
+        tmp2 = graphCpy.R3 (k)
         if tmp2 != []:
             tmp = True
             removedNodes += tmp2
         cont = tmp
-    if not remainsUndominateAttacker (graph.graphDict):
+    if not remainsUndominateAttacker (graphCpy.graphDict):
         return removedNodes
     if k == 0:
         return None
-    for node in graph.graphDict:
-        if node.color == BLACK and isinstance (node, DefNode) and len (graph.graphDict[node]) <= 7:
-            graphWithoutNode = copy.deepcopy (graph)
+    for node in graphCpy.graphDict:
+        if node.color == BLACK and isinstance (node, DefNode) and len (graphCpy.graphDict[node]) <= 7:
+            graphWithoutNode = copy.deepcopy (graphCpy)
             graphWithoutNode.removeNodeAndWhiteColorNeighboor (node)
             currentRemovedNodes = copy.deepcopy (removedNodes)
             currentRemovedNodes.append (node)
             dominatingSet = DSAP (graphWithoutNode, k-1, currentRemovedNodes)
             if dominatingSet is not None:
                 return dominatingSet
-            for neighboorNode in graph.graphDict[node]:
+            for neighboorNode in graphCpy.graphDict[node]:
                 if neighboorNode.color == BLACK and isinstance (neighboorNode, DefNode):
                     currentRemovedNodes = copy.deepcopy (removedNodes)
                     currentRemovedNodes.append (neighboorNode)
-                    graphWithoutNode = copy.deepcopy (graph)
+                    graphWithoutNode = copy.deepcopy (graphCpy)
                     graphWithoutNode.removeNodeAndWhiteColorNeighboor (neighboorNode)
                     dominatingSet = DSAP (graphWithoutNode, k-1, removedNodes)
                     if dominatingSet is not None:
--- testGraph.py
+++ testGraph.py
@@ -3,37 +3,41 @@ import copy
 import random
 from graph import Graph
 from node import BLACK, WHITE, AtkNode, DefNode
-from algo import DSAP
+from algo import DSAP, remainsUndominateAttacker
 
 XMINPOS = -10.0
 XMAXPOS = 10.0
 YMAXPOS = 10.0
 YMINPOS = -10.0
 
+
 class TestGraphMethods(unittest.TestCase):
 
-    def buildRandomGraph (self, lenght, nbMaxNeighboor):
-        g = Graph ()
-        nbAtk = random.randint (1, lenght)
+    def buildRandomGraph(self, lenght, nbMaxNeighboor):
+        g = Graph()
+        nbAtk = random.randint(1, lenght)
         nbDef = lenght - nbAtk
         for i in range(nbAtk):
-            pos = (random.uniform (XMINPOS, XMAXPOS), random.uniform (YMINPOS, YMAXPOS))
-            angle = random.uniform (0.0, 360.0)
-            node = AtkNode (pos, angle)
-            g.addNode (node)
+            pos = (random.uniform(XMINPOS, XMAXPOS),
+                   random.uniform(YMINPOS, YMAXPOS))
+            angle = random.uniform(0.0, 360.0)
+            node = AtkNode(pos, angle)
+            g.addNode(node)
         for j in range(nbDef):
-            pos = (random.uniform (XMINPOS, XMAXPOS), random.uniform (YMINPOS, YMAXPOS))
-            node = DefNode (pos)
-            g.addNode (node)
+            pos = (random.uniform(XMINPOS, XMAXPOS),
+                   random.uniform(YMINPOS, YMAXPOS))
+            node = DefNode(pos)
+            g.addNode(node)
         for node in g.graphDict:
             if g.graphDict[node] == []:
-                possibleNeighboor = random.sample (g.graphDict.keys (), random.randint (1, nbMaxNeighboor))
+                possibleNeighboor = random.sample(
+                    g.graphDict.keys(), random.randint(1, nbMaxNeighboor))
                 for neighboor in possibleNeighboor:
                     if neighboor != node:
-                        if isinstance (node, AtkNode) and isinstance (neighboor, DefNode):
-                            g.addEdge (node, neighboor)
-                        if isinstance (node, DefNode):
-                            g.addEdge (node, neighboor)
+                        if isinstance(node, AtkNode) and isinstance(neighboor, DefNode):
+                            g.addEdge(node, neighboor)
+                        if isinstance(node, DefNode):
+                            g.addEdge(node, neighboor)
         return g
 
     def testAddNode(self):
@@ -44,6 +48,15 @@ class TestGraphMethods(unittest.TestCase):
 
     def testAddEdge(self):
         return
+
+    def isDominatingSet(self, graph, dominatingSet):
+        graphCpy = copy.deepcopy(graph)
+        for node in dominatingSet:
+            if node not in graphCpy.graphDict:
+                return False
+            graphCpy.removeNodeAndWhiteColorNeighboor(node)
+
+        return not remainsUndominateAttacker(graphCpy.graphDict)
 
     def testRemoveNode(self):
         return
@@ -67,16 +80,20 @@ class TestGraphMethods(unittest.TestCase):
             DefC: [AtkB, AtkC]
         }
         g = Graph(graphDict)
-        dominatingSet = DSAP (g, 2, [])
-        expectedDominatingSet = [DefA, DefC] # Can be any couple of defenders
-        self.assertEqual (dominatingSet, expectedDominatingSet)
+        dominatingSet = DSAP(g, 2, [])
+        expectedDominatingSet = [DefA, DefC]  # Can be any couple of defenders
+        self.assertEqual(dominatingSet, expectedDominatingSet)
 
-        g.addEdge (DefA, DefB)
-        g.addEdge (DefB, DefC)
-        g.addEdge (DefA, DefC)
-        dominatingSet = DSAP (g, 999, [])
-        self.assertIsNone (dominatingSet)
+        g.addEdge(DefA, DefB)
+        g.addEdge(DefB, DefC)
+        g.addEdge(DefA, DefC)
+        dominatingSet = DSAP(g, 999, [])
+        self.assertIsNone(dominatingSet)
 
+        g = self.buildRandomGraph(60, 20)
+        dominatingSet = DSAP(g, 10)
+        if dominatingSet is not None:
+            self.assertTrue(self.isDominatingSet(g, dominatingSet))
 
     def testRemainsUndominateAttacker(self):
         return
